1a8f9bfa852349ba9a85d303c197a4303f8b52b5
ahmed <ahmedhosni803@gmail.com>
2024-12-16 17:33:24 +0200

    add color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color black = Color(0xff212121);
   static const Color coffeeColor = Color(0xffE2BE7F);
   static const Color white = Color(0xffffffff);
+  static const Color wqe = Color(0xffffffff);
 }
